741287041028f3d8804565130a32143de090f6b1
rozeridilar <rozeri.dilar@gmail.com>
2020-01-09 14:43:15 +0300

    Initial Commit

---
+++ TDDExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TDDExample
+//
+//  Created by Rozeri Dilar on 1/9/20.
+//  Copyright © 2020 Rozeri Dilar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
